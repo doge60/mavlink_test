c9a5a4bc8f2b16ee2b6be3803f758a6d9da6e2b9
doge60 <3020118317@qq.com>
2024-04-03 22:30:43 +0800

    创建mavlink测试项目以供学习用途（未烧录测试，不保证可行性）

---
+++ cmake-build-debug/CMakeFiles/mavlink_test.elf.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/STM32/mavlink_test/Core/Startup/startup_stm32f427iihx.s" "D:/STM32/mavlink_test/cmake-build-debug/CMakeFiles/mavlink_test.elf.dir/Core/Startup/startup_stm32f427iihx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F427xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/STM32/mavlink_test/Core/Inc"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "D:/STM32/mavlink_test/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "D:/STM32/mavlink_test/Drivers/CMSIS/Include"
+  "D:/STM32/mavlink_test/User/Inc"
+  "D:/STM32/mavlink_test/User/Inc/BSP_mavlink"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/STM32/mavlink_test/Core/Src/gpio.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/main.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/main.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/stm32f4xx_it.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/syscalls.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/sysmem.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/system_stm32f4xx.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/STM32/mavlink_test/Core/Src/usart.c" "CMakeFiles/mavlink_test.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "D:/STM32/mavlink_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  "D:/STM32/mavlink_test/User/Src/wtr_mavlink.c" "CMakeFiles/mavlink_test.elf.dir/User/Src/wtr_mavlink.c.obj" "gcc" "CMakeFiles/mavlink_test.elf.dir/User/Src/wtr_mavlink.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
